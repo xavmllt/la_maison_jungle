4794141f9d6c7f7730825f9d4ade0a16e0f7d6d4
xavmllt <xaviermaillot100@gmail.com>
2024-02-19 13:42:53 +0100

    Create React App

---
+++ la-maison-jungle/src/components/App.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Banner from './Banner'
+
+
+function App() {
+  return <Banner />
+}
+
+export default App;
---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,4 @@
+function Banner(){
+    return <h1>La maison jungle</h1>
+}
+export default Banner
