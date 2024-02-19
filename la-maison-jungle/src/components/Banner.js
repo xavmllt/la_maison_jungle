82cd45b4bbe6ce26dc69fdfac014b6b336ed0782
xavmllt <xaviermaillot100@gmail.com>
2024-02-19 15:22:49 +0100

    Style avec React

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,4 +1,14 @@
-function Banner(){
-    return <h1>La maison jungle</h1>
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
+function Banner() {
+    const title = "La maison jungle"
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>{title}</h1>  
+        </div>
+    );
 }
+
 export default Banner
