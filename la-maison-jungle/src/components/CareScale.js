0e050d64f47dcf325ca33f02b2b0f6f33df99e28
xavmllt <xaviermaillot100@gmail.com>
2024-02-22 18:14:53 +0100

    Prop React

---
+++ la-maison-jungle/src/components/CareScale.js
@@ -0,0 +1,16 @@
+function CareScale({ scaleValue, careType }) {
+    const range = [1, 2, 3]
+    const scaleType = careType === "light" ? '☀️' : '💧'
+
+    return (
+        <div>
+            {range.map((rangeElem) => 
+                scaleValue >= rangeElem ? (
+                    <span key={rangeElem.toString()}>{scaleType}</span>
+                ) : null
+                )}
+        </div>
+    )
+}
+
+export default CareScale
